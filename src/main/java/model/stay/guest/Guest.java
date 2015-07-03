c50f81afb22a80ed5fee426e5c1ff297f7ade9b5
yukieen <lpczclt@gmail.com>
2015-07-03 20:26:59 +0900

    幼児・小学生の判定を追加

--- src/main/java/model/stay/guest/Guest.java
+++ src/main/java/model/stay/guest/Guest.java
@@ -1,5 +1,7 @@
 package model.stay.guest;
 
+import model.stay.date.StayDate;
+
 /**
  * Created by yukieen on 2015/07/03.
  */
@@ -12,5 +14,14 @@ public class Guest {
         this.age = age;
         this.meal = meal;
         this.bedding = bedding;
+    }
+
+    boolean isKid(StayDate date) {
+        return age.value <= 6 && date.getValue().getMonthValue() <= 3;
+    }
+
+    boolean isElementarySchoolStudent(StayDate date) {
+        return (age.value >= 6 && date.getValue().getMonthValue() >= 4)
+                || (age.value <= 12 && date.getValue().getMonthValue() <= 3);
     }
 }
