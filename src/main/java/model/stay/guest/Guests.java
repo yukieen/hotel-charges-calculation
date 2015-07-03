ee76d040463c1c2592844694a2fb912759161f92
yukieen <lpczclt@gmail.com>
2015-07-03 20:27:26 +0900

    宿泊人数タイプを追加

--- src/main/java/model/stay/guest/Guests.java
+++ src/main/java/model/stay/guest/Guests.java
@@ -1,6 +1,9 @@
 package model.stay.guest;
 
+import model.stay.date.StayDate;
+
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Created by yukieen on 2015/07/03.
@@ -10,5 +13,17 @@ public class Guests {
 
     public Guests(List<Guest> values) {
         this.values = values;
+    }
+
+    NumberOfGuestType getNumberOfGuestType(StayDate date) {
+        // 幼児だけで宿泊すると０人になるけど実際なさげなので無視
+        long number = values.stream()
+                .filter(guest -> !guest.isKid(date))
+                .count();
+
+        return Stream.of(NumberOfGuestType.values())
+                .filter(type -> type.equals(Long.valueOf(number).intValue()))
+                .findFirst()
+                .orElse(NumberOfGuestType.MoreThanFour);
     }
 }
